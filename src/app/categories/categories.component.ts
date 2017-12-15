0c4797c3cabdbc9301d937dc815d4a0465b436ff
chris turina <chris.turina@gmail.com>
2017-12-15 10:37:45 -0800

    add categories to firebase

--- src/app/categories/categories.component.ts
+++ src/app/categories/categories.component.ts
@@ -1,13 +1,19 @@
 import { Component, OnInit } from '@angular/core';
 import { FirebaseListObservable } from 'angularfire2/database';
+import { Router } from '@angular/router';
+import { CategoryService } from '../category.service'
+import { Category } from '../category.model';
+
 
 @Component({
   selector: 'app-categories',
   templateUrl: './categories.component.html',
-  styleUrls: ['./categories.component.scss']
+  styleUrls: ['./categories.component.scss'],
+  providers: [CategoryService]
 })
 export class CategoriesComponent implements OnInit {
   categories: FirebaseListObservable<any[]>;
+  
   constructor() { }
 
   ngOnInit() {
